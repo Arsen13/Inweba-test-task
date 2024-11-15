683a627f3bdf90b420c220172bdb063b8dd02cc3
Kohut Arsen <kohut.arsen@gmail.com>
2024-11-15 13:31:59 +0200

    Correct some typo

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const faqItems = document.querySelectorAll(".faq-container-main-faq-item");
 
-console.log(faqItems);
-
 faqItems.forEach(item => {
     item.addEventListener("click", () => {
         faqItems.forEach((anotherItem) => {
@@ -9,7 +7,7 @@ faqItems.forEach(item => {
                 anotherItem.className = 'faq-container-main-faq-item';
                 anotherItem.querySelector(".line").style.display = 'none';
                 anotherItem.querySelector(".faq-text-container-second-paragraph").style.display = 'none';
-                anotherItem.querySelector(".faq-icon").src = "images/faq-minus-icon.png";
+                anotherItem.querySelector(".faq-icon").src = "images/faq-plus-icon.png";
             }
         }) 
 
@@ -17,7 +15,8 @@ faqItems.forEach(item => {
             item.className = 'faq-container-main-faq-item';
             item.querySelector(".line").style.display = 'none';
             item.querySelector(".faq-text-container-second-paragraph").style.display = 'none';
-            item.querySelector(".faq-icon").src = "images/faq-minus-icon.png";
+            const image = item.querySelector(".faq-icon");
+            image.src = image.src.includes("images/faq-plus-icon.png") ? "images/faq-minus-icon.png" : "images/faq-plus-icon.png";
         } else {
             item.className = 'faq-container-main-faq-item-opened';
             item.querySelector('.line').style.display = 'block';
